e5f49b827c356ec61fa7b6191a24e19818d1f283
José Edson MBS <joseedsonmbs@gmail.com>
2025-01-29 20:57:15 -0300

    add exemplo de base 64

--- signatures.py
+++ signatures.py
@@ -1,5 +1,6 @@
 import random
 from sympy import isprime, nextprime
+import base64
 
 def gerar_p_q(bits):
     p = random.getrandbits(bits)
@@ -11,3 +12,19 @@ def gerar_p_q(bits):
 p, q = gerar_p_q(1024)
 print(f"p: {p}")
 print(f"q: {q}")
+
+
+
+
+string = "Teste"
+byte_string = string.encode("utf-8")
+
+byte_base64 = base64.b64encode(byte_string)
+string_base64 = byte_base64.decode("utf-8")
+
+byte_result = base64.b64decode(byte_base64)
+string_result = byte_result.decode("utf-8")
+
+print(string)
+print(string_base64)
+print(string_result)
